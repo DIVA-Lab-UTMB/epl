d38bb2db47b6746e5651065ab4aa8f65a9db12b6
DIVA-Lab-UTMB <48330773+DIVA-Lab-UTMB@users.noreply.github.com>
2022-04-07 19:16:00 -0500

    Delete tests directory

--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(BipartiteModularityMaximization)
-
-test_check("BipartiteModularityMaximization")
--- tests/testthat/test-bipmod.R
+++
@@ -1,5 +0,0 @@
-test_that("Algorithm Correctness Simple", {
-  data(example_data,package = 'BipartiteModularityMaximization')
-  Q_Part=BipartiteModularityMaximization::bipmod(example_data)
-  expect_equal(Q_Part$MODULARITY,0.2622772)
-})
